3aa1bd89bf6839aa4c568ffc4144e10f1324f106
Thong Pham <Jack@Thongs-MacBook-Pro.local>
2017-02-11 22:19:44 -0800

    fix

--- server/server.py
+++ server/server.py
@@ -119,7 +119,8 @@ def create_user():
     else:
         conn = mysql.connect()
         cur = conn.cursor()
-        cur.execute("INSERT IGNORE INTO users (firstname, lastname, countryIsoAlpha2, city, dob, email, password) VALUES ( %s, %s, %s, %s, %s, %s, %s )", ( content["firstname"], content["lastname"], content["countryIsoAlpha2"], content["city"], content["dob"], content["email"], content["password"] ) )
+        cur.execute("INSERT INTO users (firstname, lastname, countryIsoAlpha2, city, dob, email, password) VALUES ( %s, %s, %s, %s, %s, %s, %s )", ( content["firstname"], content["lastname"], content["countryIsoAlpha2"], content["city"], content["dob"], content["email"], content["password"] ) )
+        conn.close()
         return 'Account has been created successfully'
 
 
