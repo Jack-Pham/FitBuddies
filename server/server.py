5b2799dd019d4b58a061bc31e154d7576cee3248
michael_schwarz <m.schwarz@tum.de>
2017-02-11 18:24:38 -0800

    Datbase interaction

--- server/server.py
+++ server/server.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask,json
+from flask.ext.mysql import MySQL
+import os
+import credentials
 
 app = Flask(__name__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_DATABASE_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
+app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
+app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
+mysql.init_app(app)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/users/<id>')
+def get_user(id):
+    conn = mysql.connect()
+    cur = conn.cursor()
+    query = "SELECT * FROM users WHERE id = %s"
+    cur.execute(query, id)
+    data = cur.fetchone()
+    conn.close()
+
+    if data is not None:
+        return json.jsonify({"id": data[0], "email": data[1], "points": data[2]})
+    else:
+        return json.jsonify({}),404
+        pass
 
 
 if __name__ == '__main__':
